eb0a2fdd0590372633785ada85fd607b73bf832e
ellore23 <florezvalencialorenzo@gmail.com>
2022-11-10 10:54:53 -0500

    boids keep in center

--- Boids_scripting/Assets/Boid.cs
+++ Boids_scripting/Assets/Boid.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private float speed;
+    Vector3 direction;
+    public List<Boid> boids;
+
+    public GameObject boidLeader;
+
+    // Boids close checker
+    public float centerStrength;
+    public float generalBoidsDistance;
+    Vector3 boidsPositionInScene;
+    int closenessChecker;
+
+
+    void FixedUpdate()
     {
-        
+        KeepFlockClose();
+        transform.Translate(direction * (speed *Time.deltaTime));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void KeepFlockClose()
+    {    
+        foreach(Boid boid in boids)
+        {
+            float distanceBetween = Vector3.Distance(boid.transform.position, transform.position);
+            if(distanceBetween <= generalBoidsDistance)
+            {
+                boidsPositionInScene += boid.transform.position;
+                closenessChecker++;
+            }
+        }
+
+        Vector3 averagePosition = boidsPositionInScene / closenessChecker;
+        averagePosition = averagePosition.normalized;
+        Vector3 faceDirection = (averagePosition - transform.position).normalized;
+
+        float time = centerStrength * Time.deltaTime;
+        direction = direction + time * faceDirection / (time + 1);
+        direction = direction.normalized;
+
     }
 }
